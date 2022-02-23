f2a1850084eeed11c70c2646b8a48d868eb9ddc0
Akanksharma00 <56409268+Akanksharma00@users.noreply.github.com>
2022-02-23 09:17:53 +0530

    Adding multiple routes and filtering the routes

--- app.js
+++ app.js
@@ -5,21 +5,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//Import my own files
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(shopRoutes);
 
-app.use('/',(req, res, next)=>{
-    res.send('<h1>Hello from Express!</h1>');
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found!</h1>');
 });
 
 app.listen(4000);
