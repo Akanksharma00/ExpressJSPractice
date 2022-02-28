ad80c501b9982f5654e989705808a3fa6f5019e2
Akanksharma00 <56409268+Akanksharma00@users.noreply.github.com>
2022-02-28 21:31:48 +0530

    Add product issue

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 // Node third party module import
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 //Import my own files
 const adminRoutes = require('./routes/admin');
@@ -12,13 +13,15 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(adminRoutes);
 
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found!</h1>');
+    // res.status(404).send('<h1>Page Not Found!</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(4000);
