33560b7d8de8c41a12646cceaa9f39a9de9907b1
Akanksharma00 <56409268+Akanksharma00@users.noreply.github.com>
2022-03-04 11:07:21 +0530

    Added Dynamic Routes

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
-// Node core module import
-// const http = require('http');
-
-// Node third party module import
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-//Import my own files
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const contactUsRoutes = require('./routes/contactus');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(adminRoutes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(contactUsRoutes);
+app.use(errorController.get404);
 
-app.use((req,res,next)=>{
-    // res.status(404).send('<h1>Page Not Found!</h1>');
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-});
-
-app.listen(4000);
+app.listen(3000);
